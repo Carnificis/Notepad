3dcb41a140c8cb9a9bc6914040778943c24d3e91
Jack Kelly <Carnificis@users.noreply.github.com>
2023-02-23 19:41:15 -0600

    Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,37 @@
 const app = require('express').Router();
 let db = reqire("../db/db.json");
-
+//Read
 app.get("/notes", (req, res) => {
     db = JSON.parse(fs.readFileSync("./db/db.json")) || [];
     res.json(db);
 });
 
+//create
+app.post("/notes",( req,res) => {
+    let newNoteToSave = {
+        ...req.body,
+        id: Math.floor(Math.random() * 1000000000)
+    }
+    db.push(newNoteToSave);
+    fs.writeFileSync("./db/db.json", JSON.stringify(db),function(err){
+        if(err) throw err;
+    });
+    res.json(db)
+})
+
+app.delete("/notes/:id",( req,res) => {
+    
+    let tempdbList = []
+    for(let i = 0; i < db.length; i++){
+        if(db[i].id != req.params.id){
+            tempdbList.push(db[i])
+        }
+    }
+    db = tempdbList;
+    fs.writeFileSync("./db/db.json", JSON.stringify(db),function(err){
+        if(err) throw err;
+    });
+    res.json(db)
+})
+
+module.exports = app;
